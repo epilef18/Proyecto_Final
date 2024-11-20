846d4a6cd01b61ca4f330964c770e473de63af29
Carlos Silva Diaz <crasd15@live.com>
2024-11-19 23:03:12 -0300

    registro
    - modelos
    - services
    - registro.html
    -views
    - setting

--- mi_casita/Inmuebles/views.py
+++ mi_casita/Inmuebles/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegistroUsuarioForm
 
-# Create your views here.
+
+def registro_usuario(request):
+    if request.method == "POST":
+        form = RegistroUsuarioForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Aquí podrías redirigir a otra página de éxito si lo prefieres
+            return render(
+                request, "registro.html", {"form": form, "mensaje": "Registro exitoso!"}
+            )
+    else:
+        form = (
+            RegistroUsuarioForm()
+        )  # Esto debe estar en el else para la primera vez que se carga la página
+
+    return render(request, "registro.html", {"form": form})
